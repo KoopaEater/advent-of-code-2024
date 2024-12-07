15181330c18c666eb5e6d47fa6f8f5b074cac4a7
Max Kørner Andersen <maxkandersen@gmail.com>
2024-12-07 18:54:00 +0100

    Milestone: 07-2 done

--- 07/part2.py
+++ 07/part2.py
@@ -1,12 +1,25 @@
 inputFile = open("./07/input.txt", "r")
 inputLines = list(map(lambda line: line.rstrip("\n"), inputFile.readlines()))
 
+def getValWithout(val, without):
+    assert str(val).endswith(str(without))
+    valStr = str(val)
+    withoutStr = str(without)
+    valWithoutStr = valStr[:-len(withoutStr)]
+    if len(valWithoutStr) == 0:
+        return 0
+    valWithout = int(valWithoutStr)
+    return valWithout
+
+
 def tryEq(testVal, nums):
     if len(nums) == 1:
         return testVal == nums[0]
     if tryEq(testVal - nums[-1], nums[:-1]):
         return True
-    if (testVal % nums[-1] == 0) and tryEq(testVal / nums[-1], nums[:-1]):
+    if (testVal % nums[-1] == 0) and tryEq(int(testVal / nums[-1]), nums[:-1]):
+        return True
+    if str(testVal).endswith(str(nums[-1])) and tryEq(getValWithout(testVal, nums[-1]), nums[:-1]):
         return True
     return False
 
